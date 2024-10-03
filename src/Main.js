2ec0147cbab361e010e6c7881e38048e75a60a5b
Andry <andry.sheremetew@gmail.com>
2024-10-03 04:38:47 +0300

    add heder + links + switch

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+
+
+function Main() {
+  return (
+    <div>
+      <header>
+        <div className="menu">
+        <a>About me</a>
+        <a>Service</a>
+        <a>Profile</a>
+        <a>FeedBack</a>
+        <a>Warranty</a>
+        </div>
+
+        <button className="btn">call</button>
+
+        <a href="#" target="_blank" className="icon telegram" />
+        <a href="#" target="_blank" className="icon instagram" />
+
+        <div className="switch">
+          <div className="theme light">
+
+          </div>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default Main;
